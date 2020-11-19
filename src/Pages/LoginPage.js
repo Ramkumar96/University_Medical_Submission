599e8a35ba61039122409055c211fe64cb1e44a7
Sivakumaran Ramkumar <ramkumar.s@omobio.net>
2020-11-19 09:34:36 +0530

    register login functions

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,107 +1,210 @@
 import React, { Component } from "react";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+
+import AuthService from "../services/auth.service";
+import { Redirect } from "react-router-dom";
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
 
 export default class Loginpage extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
-
-    this.handleEvent = this.handleEvent.bind(this);
+    this.state = {
+      username: "",
+      password: "",
+      loading: false,
+      message: "",
+      userReady: false,
+      loginUserType: "",
+    };
   }
 
-  componentDidMount() {}
+  // componentDidMount() {
+  //   const currentUser = AuthService.getCurrentUser();
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (prevState.name !== this.state.name) {
-      this.handler();
+  //   if (!currentUser) this.setState({ redirect: "/" });
+  //   this.setState({
+  //     currentUser: currentUser,
+  //   });
+  // }
+
+  onChangeUsername = (e) => {
+    this.setState({
+      username: e.target.value,
+    });
+  };
+
+  onChangePassword = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  handleLogin = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      message: "",
+      loading: true,
+    });
+
+    this.form.validateAll();
+
+    if (this.checkBtn.context._errors.length === 0) {
+      AuthService.login(this.state.username, this.state.password).then(
+        response => {
+       
+
+        // const currentUser = AuthService.getCurrentUser();
+
+          // if (!currentUser) this.setState({ redirect: "/" });
+          this.setState({
+            loginUserType:response.roles[0],
+            // currentUser: AuthService.getCurrentUser(),
+            userReady: true,
+          });
+          console.log(response.roles)
+          // window.location.reload();
+        }
+        ,
+        (error) => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+
+          this.setState({
+            loading: false,
+            message: resMessage,
+          });
+        }
+      );
+    } else {
+      this.setState({
+        loading: false,
+      });
     }
-  }
-
-  componentWillUnmount() {}
-
-  // Prototype methods, Bind in Constructor (ES2015)
-  handleEvent() {}
-
-  // Class Properties (Stage 3 Proposal)
-  handler = () => {
-    this.setState();
   };
 
   render() {
+    /**
+     * @desc: Redirection pages willbe set according to usertypes
+     */
+
+    const { loginUserType } = this.state;
+
+    if (this.state.userReady == true) {
+      if (loginUserType === "ROLE_ADMIN") {
+        return <Redirect to="/admindashboard" />;
+      }
+      if (loginUserType === "ROLE_USER") {
+        return <Redirect to="/studentdashboard" />;
+      }
+      if (loginUserType === "ROLE_MODERATOR") {
+        return <Redirect to="/staffdashboard" />;
+      }
+    }
     return (
       <div className="login-img3-body">
+        {/* ======= Hero Section ======= */}
+        <section id="hero" className="d-flex align-items-center ">
+          <div className="container">
+            <div className="row">
+              <div
+                className="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1"
+                data-aos="fade-up"
+                data-aos-delay={200}
+              >
+                <div className="container">
+                  <Form
+                    className="login-form"
+                    onSubmit={this.handleLogin}
+                    ref={(c) => {
+                      this.form = c;
+                    }}
+                  >
+                    <div className="login-wrap">
+                      <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <Input
+                          type="text"
+                          className="form-control"
+                          name="username"
+                          value={this.state.username}
+                          onChange={this.onChangeUsername}
+                          validations={[required]}
+                        />
+                      </div>
 
-            {/* ======= Hero Section ======= */}
-            <section id="hero" className="d-flex align-items-center ">
-            <div className="container">
-              <div className="row">
-                <div
-                  className="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1"
-                  data-aos="fade-up"
-                  data-aos-delay={200}
-                >
-                  <div className="container">
-            <form className="login-form" action="index.html">
-              <div className="login-wrap">
-                <p className="login-img">
-                  <i className="icon_lock_alt" />
-                </p>
-                <div className="input-group">
-                  <span className="input-group-addon">
-                    <i className="icon_profile" />
-                  </span>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Username"
-                    autofocus
-                  />
+                      <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <Input
+                          type="password"
+                          className="form-control"
+                          name="password"
+                          value={this.state.password}
+                          onChange={this.onChangePassword}
+                          validations={[required]}
+                        />
+                      </div>
+
+                      <div className="form-group">
+                        <button
+                          className="btn btn-primary btn-block"
+                          disabled={this.state.loading}
+                        >
+                          {this.state.loading && (
+                            <span className="spinner-border spinner-border-sm"></span>
+                          )}
+                          <span>Login</span>
+                        </button>
+                      </div>
+
+                      {this.state.message && (
+                        <div className="form-group">
+                          <div className="alert alert-danger" role="alert">
+                            {this.state.message}
+                          </div>
+                        </div>
+                      )}
+                      <CheckButton
+                        style={{ display: "none" }}
+                        ref={(c) => {
+                          this.checkBtn = c;
+                        }}
+                      />
+                    </div>
+                  </Form>
                 </div>
-                <div className="input-group">
-                  <span className="input-group-addon">
-                    <i className="icon_key_alt" />
-                  </span>
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Password"
-                  />
-                </div>
-                <label className="checkbox">
-                  <input type="checkbox" defaultValue="remember-me" /> Remember
-                  me
-                  <span className="pull-right">
-                    {" "}
-                    <a href="#"> Forgot Password?</a>
-                  </span>
-                </label>
-                <button
-                  className="btn btn-primary btn-lg btn-block"
-                  type="submit"
-                >
-                  Login
-                </button>
               </div>
-            </form>
-          </div>
-                  
-                </div>
-                <div
-                  className="col-lg-6 order-1 order-lg-2 hero-img"
-                  data-aos="zoom-in"
-                  data-aos-delay={200}
-                >
-                  <img
-                    src="assets/img/hero-img.png"
-                    className="img-fluid animated"
-                    alt
-                  />
-                </div>
+              <div
+                className="col-lg-6 order-1 order-lg-2 hero-img"
+                data-aos="zoom-in"
+                data-aos-delay={200}
+              >
+                <img
+                  src="assets/img/hero-img.png"
+                  className="img-fluid animated"
+                  alt
+                />
               </div>
             </div>
-          </section>
-          {/* End Hero */}
-       
+          </div>
+        </section>
+        {/* End Hero */}
       </div>
     );
   }
--- src/components/Admin/AddUser.js
+++ src/components/Admin/AddUser.js
@@ -1,31 +1,148 @@
 import React, { Component } from "react";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import { isEmail } from "validator";
+
+import AuthService from "../../services/auth.service";
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
+
+const email = (value) => {
+  if (!isEmail(value)) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This is not a valid email.
+      </div>
+    );
+  }
+};
+
+const vusername = (value) => {
+  if (value.length < 3 || value.length > 20) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        The username must be between 3 and 20 characters.
+      </div>
+    );
+  }
+};
 
 export default class Adduser extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
-
-    this.handleEvent = this.handleEvent.bind(this);
+    this.state = {
+      userId: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+      mobile: "",
+      address: "",
+      userRole: "",
+      userName: null ,
+      password: null,
+      successful: false,
+      message: "",
+    };
   }
 
-  componentDidMount() {}
+  onChangeuserId = (e) => {
+    this.setState({
+      userId: e.target.value,
+    });
+  };
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (prevState.name !== this.state.name) {
-      this.handler();
+  onChangefirstName = (e) => {
+    this.setState({
+      firstName: e.target.value,
+    });
+  };
+  onChangelastName = (e) => {
+    this.setState({
+      lastName: e.target.value,
+    });
+  };
+  onChangeEmail = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+  onChangemobile = (e) => {
+    this.setState({
+      mobile: e.target.value,
+    });
+  };
+  onChangeaddress = (e) => {
+    this.setState({
+      address: e.target.value,
+    });
+  };
+  onChangeuserRole = (e) => {
+    this.setState({
+      userRole: e.target.value,
+    });
+  };
+
+  handleRegister=(e)=> {
+    e.preventDefault();
+
+    this.setState({
+      message: "",
+      successful: false,
+    });
+
+
+    this.form.validateAll();
+
+    if (this.checkBtn.context._errors.length === 0) {
+      
+    console.log(
+      this.state.userId, 
+      this.state.firstName, 
+      this.state.lastName,
+      this.state.email,
+      this.state.mobile,
+      this.state.address,
+      this.state.userRole,
+      this.state.userName,
+      this.state.password,
+      );
+      // AuthService.register(
+      //   this.state.username,
+      //   this.state.email,
+      //   this.state.password
+      // ).then(
+      //   (response) => {
+      //     this.setState({
+      //       message: response.data.message,
+      //       successful: true,
+      //     });
+      //   },
+      //   (error) => {
+      //     const resMessage =
+      //       (error.response &&
+      //         error.response.data &&
+      //         error.response.data.message) ||
+      //       error.message ||
+      //       error.toString();
+
+      //     this.setState({
+      //       successful: false,
+      //       message: resMessage,
+      //     });
+      //   }
+      // );
     }
   }
-
-  componentWillUnmount() {}
-
-  // Prototype methods, Bind in Constructor (ES2015)
-  handleEvent() {}
-
-  // Class Properties (Stage 3 Proposal)
-  handler = () => {
-    this.setState();
-  };
 
   render() {
     return (
@@ -36,153 +153,188 @@ export default class Adduser extends Component {
               <i className="fa fa-files-o" />
               Add New Users
             </h3>
-            <ol className="breadcrumb">
-              <li>
-                <i className="fa fa-home" />
-                <a href="index.html">Home</a>
-              </li>
-              <li>
-                <i className="fa fa-files-o" />
-                Add New Users
-              </li>
-            </ol>
           </div>
         </div>
         <div className="row">
           <div className="col-lg-12">
             <section className="panel">
-              <header className="panel-heading">
-                Advanced Form validations
-              </header>
+              <header className="panel-heading">Add new users</header>
               <div className="panel-body">
                 <div className="form">
-                  <form
+                  <Form
                     className="form-validate form-horizontal "
-                    id="register_form"
-                    method="get"
-                    action
+                    onSubmit={this.handleRegister}
+                    ref={(c) => {
+                      this.form = c;
+                    }}
                   >
-                    <div className="form-group ">
-                      <label
-                        htmlFor="fullname"
-                        className="control-label col-lg-2"
-                      >
-                        Full name <span className="required">*</span>
-                      </label>
-                      <div className="col-lg-10">
-                        <input
-                          className=" form-control"
-                          id="fullname"
-                          name="fullname"
-                          type="text"
-                        />
+                    {!this.state.successful && (
+                      <div>
+                        <div className="form-group ">
+                          <label
+                            htmlFor="userid"
+                            className="control-label col-lg-2"
+                          >
+                            User ID<span className="required">*</span>
+                          </label>
+                          <div className="col-lg-10">
+                            <Input
+                              type="text"
+                              className="form-control"
+                              name="userid"
+                              value={this.state.userId}
+                              onChange={this.onChangeuserId}
+                              validations={[required]}
+                            />
+                          </div>
+                        </div>
+                        <div className="form-group ">
+                          <label
+                            htmlFor="firstname"
+                            className="control-label col-lg-2"
+                          >
+                            First name <span className="required">*</span>
+                          </label>
+                          <div className="col-lg-10">
+                            <Input
+                              type="text"
+                              className="form-control"
+                              name="firstname"
+                              value={this.state.firstName}
+                              onChange={this.onChangefirstName}
+                              validations={[required]}
+                            />
+                          </div>
+                        </div>
+                        <div className="form-group ">
+                          <label
+                            htmlFor="lastname"
+                            className="control-label col-lg-2"
+                          >
+                            Last name <span className="required">*</span>
+                          </label>
+                          <div className="col-lg-10">
+                            <Input
+                              type="text"
+                              className="form-control"
+                              name="lastname"
+                              value={this.state.lastName}
+                              onChange={this.onChangelastName}
+                              validations={[required]}
+                            />
+                          </div>
+                        </div>
+                        <div className="form-group ">
+                          <label
+                            htmlFor="email"
+                            className="control-label col-lg-2"
+                          >
+                            Email <span className="required">*</span>
+                          </label>
+                          <div className="col-lg-10">
+                            <Input
+                              type="text"
+                              className="form-control"
+                              name="email"
+                              value={this.state.email}
+                              onChange={this.onChangeEmail}
+                              validations={[required, email]}
+                            />
+                          </div>
+                        </div>
+                        <div className="form-group ">
+                          <label
+                            htmlFor="mobile"
+                            className="control-label col-lg-2"
+                          >
+                            Mobile <span className="required">*</span>
+                          </label>
+                          <div className="col-lg-10">
+                            <Input
+                              type="text"
+                              className="form-control"
+                              name="mobile"
+                              value={this.state.mobile}
+                              onChange={this.onChangemobile}
+                              validations={[required]}
+                            />
+                          </div>
+                        </div>
+                        <div className="form-group ">
+                          <label
+                            htmlFor="address"
+                            className="control-label col-lg-2"
+                          >
+                            Address <span className="required">*</span>
+                          </label>
+                          <div className="col-lg-10">
+                            <Input
+                              type="text"
+                              className="form-control"
+                              name="address"
+                              value={this.state.address}
+                              onChange={this.onChangeaddress}
+                              validations={[required]}
+                            />
+                          </div>
+                        </div>
+                        <div className="form-group ">
+                          <label
+                            htmlFor="userid"
+                            className="control-label col-lg-2"
+                          >
+                            User Role<span className="required">*</span>
+                          </label>
+                          <div className="col-lg-10">
+                            <select
+                              class="form-control input-sm m-bot15"
+                              value={this.state.userRole}
+                              onChange={this.onChangeuserRole}
+                            >
+                              <option defaultValue> Select user Role </option>
+                              <option userrole="student">Student</option>
+                              <option userrole="staff">Staff</option>
+                              <option userrole="lecturer">Lecturer</option>
+                              <option userrole="hod">HOD</option>
+                              <option userrole="dean">Dean</option>
+                              <option userrole="admin">Admin</option>
+                            </select>
+                          </div>
+                        </div>
+
+                        <div className="form-group">
+                          <div className="col-lg-offset-2 col-lg-10">
+                            <button
+                              className="btn btn-primary btn-block"
+                            >
+                              Save
+                            </button>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                    <div className="form-group ">
-                      <label
-                        htmlFor="address"
-                        className="control-label col-lg-2"
-                      >
-                        Address <span className="required">*</span>
-                      </label>
-                      <div className="col-lg-10">
-                        <input
-                          className=" form-control"
-                          id="address"
-                          name="address"
-                          type="text"
-                        />
+                    )}
+
+                    {this.state.message && (
+                      <div className="form-group">
+                        <div
+                          className={
+                            this.state.successful
+                              ? "alert alert-success"
+                              : "alert alert-danger"
+                          }
+                          role="alert"
+                        >
+                          {this.state.message}
+                        </div>
                       </div>
-                    </div>
-                    <div className="form-group ">
-                      <label
-                        htmlFor="username"
-                        className="control-label col-lg-2"
-                      >
-                        Username <span className="required">*</span>
-                      </label>
-                      <div className="col-lg-10">
-                        <input
-                          className="form-control "
-                          id="username"
-                          name="username"
-                          type="text"
-                        />
-                      </div>
-                    </div>
-                    <div className="form-group ">
-                      <label
-                        htmlFor="password"
-                        className="control-label col-lg-2"
-                      >
-                        Password <span className="required">*</span>
-                      </label>
-                      <div className="col-lg-10">
-                        <input
-                          className="form-control "
-                          id="password"
-                          name="password"
-                          type="password"
-                        />
-                      </div>
-                    </div>
-                    <div className="form-group ">
-                      <label
-                        htmlFor="confirm_password"
-                        className="control-label col-lg-2"
-                      >
-                        Confirm Password <span className="required">*</span>
-                      </label>
-                      <div className="col-lg-10">
-                        <input
-                          className="form-control "
-                          id="confirm_password"
-                          name="confirm_password"
-                          type="password"
-                        />
-                      </div>
-                    </div>
-                    <div className="form-group ">
-                      <label htmlFor="email" className="control-label col-lg-2">
-                        Email <span className="required">*</span>
-                      </label>
-                      <div className="col-lg-10">
-                        <input
-                          className="form-control "
-                          id="email"
-                          name="email"
-                          type="email"
-                        />
-                      </div>
-                    </div>
-                    <div className="form-group ">
-                      <label
-                        htmlFor="agree"
-                        className="control-label col-lg-2 col-sm-3"
-                      >
-                        Agree to Our Policy <span className="required">*</span>
-                      </label>
-                      <div className="col-lg-10 col-sm-9">
-                        <input
-                          type="checkbox"
-                          style={{ width: 20 }}
-                          className="checkbox form-control"
-                          id="agree"
-                          name="agree"
-                        />
-                      </div>
-                    </div>
-                    <div className="form-group">
-                      <div className="col-lg-offset-2 col-lg-10">
-                        <button className="btn btn-primary" type="submit">
-                          Save
-                        </button>
-                        <button className="btn btn-default" type="button">
-                          Cancel
-                        </button>
-                      </div>
-                    </div>
-                  </form>
+                    )}
+                    <CheckButton
+                      style={{ display: "none" }}
+                      ref={(c) => {
+                        this.checkBtn = c;
+                      }}
+                    />
+                  </Form>
                 </div>
               </div>
             </section>
